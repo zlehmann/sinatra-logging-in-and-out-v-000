0047ec6f223497865a3c00b21d569b83cc3a2eb3
Zachary Lehmann <zlehmann@gmail.com>
2018-12-21 15:12:36 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,6 +23,7 @@ class ApplicationController < Sinatra::Base
 
   get '/account' do
     puts params
+    puts session
     @is_logged_in = Helpers.is_logged_in?(session)
     if @is_logged_in != true
       redirect '/error'
