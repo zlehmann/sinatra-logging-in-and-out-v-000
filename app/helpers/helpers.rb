228a6e5f13f3035c18c99f97b860ea0e1f62292e
Zachary Lehmann <zlehmann@gmail.com>
2018-12-21 14:32:53 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -2,13 +2,13 @@ require 'pry'
 class Helpers
 
   def self.current_user(session)
-    @user = User.find_by(id: session[:user_id])
+    user = User.find_by(id: session[:user_id])
   end
 
   def self.is_logged_in?(session)
-    @user = User.find_by(id: session[:user_id])
+    user = User.find_by(id: session[:user_id])
     result = nil
-    if session[:user_id] == @user.id
+    if session[:user_id] == user.id
       result = true
     else
       result = false
